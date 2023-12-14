6ca988754d4a7f8dfa1587a8834e08fc2e80b471
Lucas1774 <lucas.115@hotmail.com>
2023-12-15 00:29:15 +0100

    Add spacing in prints

--- src/functionOptimizer.py
+++ src/functionOptimizer.py
@@ -11,18 +11,22 @@ def main():
     print("-------------Imported data-------------")
     data = ExcelData(input_file)
     DataManager.print(data)
+    print()
 
     print("-------------Selected individuals for variant-1-------------")
     DataManager.do_selection_variant_1(data)
     print(data.get_selected_individuals("variante-1"))
+    print()
 
     print("-------------Crossing data for variant-1-------------")
     DataManager.do_cross_variant_1(data)
     print(data.get_crossing_by_pairs("variante-1"))
+    print()
 
     print("-------------Next generation's genes-------------")
     DataManager.do_next_generation_variant_1(data)
     print(data.get_next_generation("variante-1"))
+    print()
 
 
 if __name__ == "__main__":
