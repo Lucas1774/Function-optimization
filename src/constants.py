780006b742ae2ac337cbe21e1d689aa995954c3a
Lucas1774 <lucas.115@hotmail.com>
2023-12-17 03:29:23 +0100

    Implement iteration

--- src/constants.py
+++ src/constants.py
@@ -1,22 +1,25 @@
 class Constants:
-    _FILE_PATH = "input.xls"
+    _FILE_PATH = "input.xlsx"
     _INDEXES = {
         "variante-1": {
             "POPULATION": [[5, 2], [14, 14]],
             "RANDOM_NUMBERS": [[3, 17], [34, 21]],
+            "GENERATOR": [[18, 2], [26, 13]],
         },
         "variante-2": {
             "POPULATION": [[5, 2], [20, 8]],
             "RANDOM_NUMBERS": [[3, 11], [34, 15]],
+            "GENERATOR": [[23, 2], [32, 7]],
         },
     }
+    _NUMBER_OF_ITERATIONS = 100
 
     _CROSS_PROBABILITY = 0.47
-    _MUTATION_PROBABILITY = 0.29
+    _MUTATION_PROBABILITY = 0.5
 
     _NUMBER_OF_TOURNAMENTS = 4
     _CROSS_PROBABILITY_V2 = 0.73
-    _MUTATION_PROBABILITY_V2 = 0.17
+    _MUTATION_PROBABILITY_V2 = 0.3
     _MIN_MUTATION_VALUE = -9
     _MAX_MUTATION_VALUE = 7
 
@@ -29,29 +32,33 @@ class Constants:
         return self._INDEXES
 
     @property
+    def NUMBER_OF_ITERATIONS(self):
+        return self._NUMBER_OF_ITERATIONS
+
+    @property
     def CROSS_PROBABILITY(self):
         return self._CROSS_PROBABILITY
 
     @property
     def MUTATION_PROBABILITY(self):
         return self._MUTATION_PROBABILITY
-    
+
     @property
     def NUMBER_OF_TOURNAMENTS(self):
         return self._NUMBER_OF_TOURNAMENTS
-    
+
     @property
     def CROSS_PROBABILITY_V2(self):
         return self._CROSS_PROBABILITY_V2
-    
+
     @property
     def MUTATION_PROBABILITY_V2(self):
         return self._MUTATION_PROBABILITY_V2
-    
+
     @property
     def MIN_MUTATION_VALUE(self):
         return self._MIN_MUTATION_VALUE
-    
+
     @property
     def MAX_MUTATION_VALUE(self):
         return self._MAX_MUTATION_VALUE
--- src/functionOptimizer.py
+++ src/functionOptimizer.py
@@ -2,81 +2,108 @@ from dataManagerV1 import DataManagerV1
 from dataManagerV2 import DataManagerV2
 from dataSourceHandler import DataSourceHandler
 from excelData import ExcelData
+from constants import Constants
 
 
 def main():
+    def run():
+        print("-------------Imported data-------------")
+        DataManagerV1.print(data)
+        print()
+
+        print("-------------Selected individuals for variant-1-------------")
+        DataManagerV1.do_selection(data)
+        print(data.get_selected_individuals("variante-1"))
+        print()
+
+        print("-------------Crossing data for variant-1-------------")
+        DataManagerV1.do_cross(data)
+        print(data.get_crossing_by_pairs("variante-1"))
+        print()
+
+        print("-------------Next generation's genes for variant-1-------------")
+        DataManagerV1.do_next_generation(data)
+        print(data.get_next_generation("variante-1"))
+        print()
+
+        print("-------------Mutated genes for variant-1-------------")
+        DataManagerV1.do_mutation(data)
+        print(data.get_mutated_genes("variante-1"))
+        print()
+
+        print(
+            "-------------Next generation's genes after mutation for variant-1-------------"
+        )
+        DataManagerV1.do_next_generation_finish(data)
+        print(data.get_next_generation_mutated("variante-1"))
+        print()
+
+        print("-------------Next generation for variant-1-------------")
+        DataManagerV1.do_replacement(data)
+        print(data.get_next_generation_finished("variante-1"))
+        print()
+
+        print("-------------Selected individuals for variant-2-------------")
+        DataManagerV2.do_selection(data)
+        print(data.get_selected_individuals("variante-2"))
+        print()
+
+        print("-------------Crossing data for variant-2-------------")
+        DataManagerV2.do_cross(data)
+        print(data.get_crossing_by_pairs("variante-2"))
+        print()
+
+        print("-------------Next generation's genes for variant-2-------------")
+        DataManagerV2.do_next_generation(data)
+        print(data.get_next_generation("variante-2"))
+        print()
+
+        print("-------------Mutated genes for variant-2-------------")
+        DataManagerV2.do_mutation(data)
+        print(data.get_mutated_genes("variante-2"))
+        print()
+
+        print(
+            "-------------Next generation's genes after mutation for variant-1-------------"
+        )
+        DataManagerV2.do_next_generation_finish(data)
+        print(data.get_next_generation_mutated("variante-2"))
+        print()
+
+        print("-------------Next generation for variant-2-------------")
+        DataManagerV2.do_replacement(data)
+        print(data.get_next_generation_finished("variante-2"))
+        print()
+
+        print(f"Attempting to write variant-1 in {file_path}")
+        DataSourceHandler.write_excel_file(
+            file_path=file_path,
+            sheet_name="variante-1",
+            data=data.get_next_generation_finished("variante-1"),
+            start_row=constants.INDEXES["variante-1"]["GENERATOR"][0][0] + 1,
+            start_col=constants.INDEXES["variante-1"]["GENERATOR"][0][1] + 1,
+        )
+        print(f"Attempting to write variant-2 in {file_path}")
+        DataSourceHandler.write_excel_file(
+            file_path=file_path,
+            sheet_name="variante-2",
+            data=data.get_next_generation_finished("variante-2"),
+            start_row=constants.INDEXES["variante-2"]["GENERATOR"][0][0] + 1,
+            start_col=constants.INDEXES["variante-2"]["GENERATOR"][0][1] + 1,
+        )
+
+    constants = Constants()
     file_path = DataSourceHandler.get_file_path()
-    print(f"Attempting to read {file_path}")
-    input_file = DataSourceHandler.read_excel_file(file_path)
 
-    print("-------------Imported data-------------")
-    data = ExcelData(input_file)
-    DataManagerV1.print(data)
-    print()
-
-    print("-------------Selected individuals for variant-1-------------")
-    DataManagerV1.do_selection(data)
-    print(data.get_selected_individuals("variante-1"))
-    print()
-
-    print("-------------Crossing data for variant-1-------------")
-    DataManagerV1.do_cross(data)
-    print(data.get_crossing_by_pairs("variante-1"))
-    print()
-
-    print("-------------Next generation's genes for variant-1-------------")
-    DataManagerV1.do_next_generation(data)
-    print(data.get_next_generation("variante-1"))
-    print()
-
-    print("-------------Mutated genes for variant-1-------------")
-    DataManagerV1.do_mutation(data)
-    print(data.get_mutated_genes("variante-1"))
-    print()
-
-    print(
-        "-------------Next generation's genes after mutation for variant-1-------------"
-    )
-    DataManagerV1.do_next_generation_finish(data)
-    print(data.get_next_generation_mutated("variante-1"))
-    print()
-
-    print("-------------Next generation for variant-1-------------")
-    DataManagerV1.do_replacement(data)
-    print(data.get_next_generation_finished("variante-1"))
-    print()
-
-    print("-------------Selected individuals for variant-2-------------")
-    DataManagerV2.do_selection(data)
-    print(data.get_selected_individuals("variante-2"))
-    print()
-
-    print("-------------Crossing data for variant-2-------------")
-    DataManagerV2.do_cross(data)
-    print(data.get_crossing_by_pairs("variante-2"))
-    print()
-
-    print("-------------Next generation's genes for variant-2-------------")
-    DataManagerV2.do_next_generation(data)
-    print(data.get_next_generation("variante-2"))
-    print()
-
-    print("-------------Mutated genes for variant-2-------------")
-    DataManagerV2.do_mutation(data)
-    print(data.get_mutated_genes("variante-2"))
-    print()
-
-    print(
-        "-------------Next generation's genes after mutation for variant-1-------------"
-    )
-    DataManagerV2.do_next_generation_finish(data)
-    print(data.get_next_generation_mutated("variante-2"))
-    print()
-
-    print("-------------Next generation for variant-2-------------")
-    DataManagerV2.do_replacement(data)
-    print(data.get_next_generation_finished("variante-2"))
-    print()
+    for i in range(constants._NUMBER_OF_ITERATIONS):
+        print(f"Attempting to read {file_path}")
+        input_file = DataSourceHandler.read_excel_file(file_path)
+        if i == 0:
+            data = ExcelData(input_file)
+        else:
+            data.set_data("variante-1", input_file)
+            data.set_data("variante-2", input_file)
+        run()
 
 
 if __name__ == "__main__":
